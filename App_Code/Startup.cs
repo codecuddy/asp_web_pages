6c161737a3b39693183d532c0754f4826cf32fc4
Charlie Cuddy <charliecuddy4@gmail.com>
2017-02-19 21:58:09 -0600

    initial commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web_pages.Startup))]
+namespace web_pages
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
